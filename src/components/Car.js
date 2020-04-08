58778ae0ebb7eac7dbc57db0379b9d5e244535b0
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-08 09:59:13 -0500

    cars card working and looks good

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,14 +1,40 @@
-import React from 'react'
-import cars from '../cars.json'
-// import material ui components here //
-// Container, Paper, Chip //
+import React from 'react';
+import cars from '../cars.json';
+import { Typography, Container, Paper, Chip } from '@material-ui/core';
 
 const Car = (props) => {
     const id = Number(props.match.params.id);
     const car = cars.find(car => car.id === id);
     console.log(car);
     return (
-        <h1>{car.Name}</h1>
+        <Container 
+            maxWidth="sm" 
+            style={{marginTop: "55px"}}>
+            <Paper style={{
+                display: "flex",
+                flexWrap: "wrap",
+                alignItems: "center",
+                justifyContent: "flex-start",
+                height: "35vh",
+                padding: "25px 10px"
+                }}>
+                <Container>
+                    <Typography variant="h3">{car.Name}</Typography>
+                </Container>
+                <Container style={{}}>
+                    <Chip label={`id: ${car.id}`} style={{margin: "0"}}/>
+                    <Chip label={`Name: ${car.Name}`} />
+                    <Chip label={`Miles per gallon: ${car.Miles_per_Gallon}`} />
+                    <Chip label={`Cylinders: ${car.Cylinders}`} />
+                    <Chip label={`Displacement: ${car.Displacement}`} />
+                    <Chip label={`Horsepower: ${car.Horsepower}`} />
+                    <Chip label={`Weight in lbs: ${car.Weight_in_lbs}`} />
+                    <Chip label={`Acceleration: ${car.Acceleration}`} />
+                    <Chip label={`Year: ${car.Year}`} />
+                    <Chip label={`Origin: ${car.Origin}`} />
+                </Container>
+            </Paper>
+        </Container>
     )
 }
 
