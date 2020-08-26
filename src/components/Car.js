649065690260304b5438fa169bf501cc4767da25
evantwidwell <evanski@gmail.com>
2020-08-25 20:55:18 -0500

    wk4d1

--- src/components/Car.js
+++ src/components/Car.js
@@ -2,11 +2,29 @@ import React from 'react'
 import cars from '../cars.json'
 // import material ui components here //
 // Container, Paper, Chip //
-
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
+console.log("In the car component")
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
+    console.log("In the car component")
+    let id = props.match.params.id
+    
+    return(
+    <div>
+        <Container maxWidth="xs">
+            <Paper>
+                <h3>{cars[id-1].Name}</h3><br/>
+    <Chip label={`Name: ${cars[id-1].Name}`}/>
+    <Chip label={`ID: ${cars[id-1].id}`}/>
+    <Chip label={`MPG: ${cars[id-1].Miles_per_Gallon}`}/>
+    <Chip label={`Cylinders: ${cars[id-1].Cylinders}`}/>
+            </Paper>
+        </Container>
+    </div>
     )
+    
+    
 }
 
 export default Car
