bec3b913badae18fd9c50599835ef5797d2c9b17
Aaron Cook <boredtodeath0321@gmail.com>
2021-04-23 12:17:02 -0500

    finished

--- src/Router.js
+++ src/Router.js
@@ -12,6 +12,19 @@ import React from 'react'
 import { Switch, Route } from 'react-router'
 // Write component imports here //
 
-
+import Home from './components/Home';
+import About from './components/About';
+import Car from './components/Car';
 
 // Start Router function here //
+const Router = () => {
+    return (
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/car/:id" exact component={Car} />
+        </Switch>
+    )
+}
+
+export default Router
